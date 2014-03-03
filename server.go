e331e8b7b4ee9c616c162cd624294b2efb7b5cea
Dawid Pogorzelski <shogun999@gmail.com>
2014-03-03 22:36:17 +0100

    added basic error handling + goroutines #1

--- server.go
+++ server.go
@@ -38,7 +38,15 @@ func remoteHandler(res http.ResponseWriter, req *http.Request) {
 
 	//when someone requires a ws connection we create a new player and store a
 	// pointer to the connection inside player.Socket
-	ws, _ := websocket.Upgrade(res, req, nil, 1024, 1024)
+	ws, err := websocket.Upgrade(res, req, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(res, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
+		return
+	}
+
 	log.Printf("got websocket conn from %v\n", ws.RemoteAddr())
 	player := new(Player)
 	player.Id = uuid.New()
@@ -48,12 +56,19 @@ func remoteHandler(res http.ResponseWriter, req *http.Request) {
 	// players (if any) and viceversa, we tell the player where to spawn already
 	// existing players
 	log.Println("Publishing positions")
-	for _, p := range Players {
-		if p.Socket.RemoteAddr() != player.Socket.RemoteAddr() {
-			player.Socket.WriteJSON(p.position(true))
-			p.Socket.WriteJSON(player.position(true))
+
+	go func() {
+		for _, p := range Players {
+			if p.Socket.RemoteAddr() != player.Socket.RemoteAddr() {
+				if err = player.Socket.WriteJSON(p.position(true)); err != nil {
+					log.Println(err)
+				}
+				if err = p.Socket.WriteJSON(player.position(true)); err != nil {
+					log.Println(err)
+				}
+			}
 		}
-	}
+	}()
 
 	// we append the new player to Players slice
 	Players = append(Players, player)
@@ -62,13 +77,15 @@ func remoteHandler(res http.ResponseWriter, req *http.Request) {
 		// the other players know to despawn his sprite (Online: false) and
 		// remove him from the slice so no further updates will be sent
 		if err = player.Socket.ReadJSON(&player); err != nil {
-			log.Println("Player Disconnected waiting", err.Error())
+			log.Println("Player Disconnected waiting", err)
 			for i, p := range Players {
 				if p.Socket.RemoteAddr() == player.Socket.RemoteAddr() {
 					Players = append(Players[:i], Players[i+1:]...)
 				} else {
 					log.Println("destroy player", player)
-					p.Socket.WriteJSON(Message{Online: false, Id: player.Id})
+					if err = p.Socket.WriteJSON(Message{Online: false, Id: player.Id}); err != nil {
+						log.Println(err)
+					}
 				}
 			}
 			log.Println("Number of players still connected ...", len(Players))
@@ -77,12 +94,16 @@ func remoteHandler(res http.ResponseWriter, req *http.Request) {
 
 		// a regular broadcast to inform all the players about a player's
 		// position update
-		for _, p := range Players {
-			if p.Socket.RemoteAddr() != player.Socket.RemoteAddr() {
-				p.Socket.WriteJSON(player.position(false))
-			}
+		go func() {
+			for _, p := range Players {
+				if p.Socket.RemoteAddr() != player.Socket.RemoteAddr() {
+					if err = p.Socket.WriteJSON(player.position(false)); err != nil {
+						log.Println(err)
+					}
+				}
 
-		}
+			}
+		}()
 	}
 }
 
